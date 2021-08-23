9fcf54a6a56e27742c6588f926feb8e8712425c5
caugusta <caugusta@student.21-school.ru>
2021-08-23 23:31:59 +0300

    ex00 complete

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caugusta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/23 22:10:50 by caugusta          #+#    #+#             */
+/*   Updated: 2021/08/23 23:30:56 by caugusta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	int	i = 1;
+	int	j = 0;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{	
+			std::cout << (char)toupper((int)argv[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
